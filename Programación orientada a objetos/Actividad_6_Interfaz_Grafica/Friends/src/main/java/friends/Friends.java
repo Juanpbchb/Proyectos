d2a11856eea52bf5e5d483d070b90ce805a3afc9
Juanpbchb <131199500+Juanpbchb@users.noreply.github.com>
2024-09-18 20:22:48 -0500

    Add files via upload

---
+++ Programación orientada a objetos/Actividad_6_Interfaz_Grafica/Friends/src/main/java/friends/Friends.java
@@ -0,0 +1,14 @@
+package friends;
+
+/**
+ *
+ * @author Juan Pablo Chamorro Bolaños
+ */
+public class Friends {
+
+    public static void main(String[] args) {
+       MainForm frm = new MainForm();
+       frm.setVisible(true);
+               
+    }
+}
